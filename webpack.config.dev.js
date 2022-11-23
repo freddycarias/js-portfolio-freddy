72aff15b74d7e96485b382120cab34bf2cfdbdcd
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-23 14:57:54 -0600

    Webpack Dev Server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext]',
     },
     mode: "development",
-    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -78,7 +77,16 @@ module.exports = {
         }),
         new Dotenv(),
     ],
-    optimization: {
-        minimize: true
-    }
+    devServer: {
+        static: 
+        {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true, //Hace que se abra en el navegador
+	}
 }
