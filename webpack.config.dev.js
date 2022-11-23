27067a75cfec8b87a20534b2f364890f207d40eb
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-23 15:22:42 -0600

    Webpack DevTools

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext]',
     },
     mode: "development",
+    devtool: "source-map" ,
     resolve: {
         extensions: ['.js'],
         alias: {
