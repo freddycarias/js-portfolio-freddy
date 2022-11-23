1dd65e7bd574a9036bad6147b3ae6a69e3ed2f42
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-23 10:39:23 -0600

    Webpack Watch

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext]',
     },
     mode: "development",
+    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
